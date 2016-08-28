cd96d8b3d6767734953811fd8c18da06d1654136
Kate Shaffer <kate.shaffer@gmail.com>
2016-08-27 23:23:52 -0700

    Looks like I updated leap.rb sometime...

--- 01-leap/leap.rb
+++ 01-leap/leap.rb
@@ -1,11 +1,19 @@
+# def leap_year?(year)
+#   if year % 4 == 0 && year % 100 != 0
+#     return true
+#   else
+#     if year % 400 == 0
+#       return true
+#     else
+#       return false
+#     end
+#   end
+# end
+
 def leap_year?(year)
-  if year % 4 == 0 && year % 100 != 0
+  if year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
     return true
   else
-    if year % 400 == 0
-      return true
-    else
-      return false
-    end
+    return false
   end
 end
